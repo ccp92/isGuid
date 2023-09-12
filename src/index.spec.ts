dabd356400dee7526054a20511b3aceebfa29fed
ccp92 <mail@chrisparsons.dev>
2023-09-12 15:01:59 +0100

    feat: add optional parameter to pass in version for the check

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,9 +2,57 @@ import checkIsGuid from "./index";
 
 describe("isGuid", () => {
   it("should return true if the value is a valid GUID", () => {
-    expect(checkIsGuid("00000000-0000-0000-0000-000000000000")).toBe(true);
+    expect(checkIsGuid("f4090a94-1774-46d7-b469-109ddec68cd2")).toBe(true);
   });
+
   it("should return false if the value is not a valid GUID", () => {
-    expect(checkIsGuid("00000000-0000-0000-0000")).toBe(false);
+    expect(checkIsGuid("f4090a94-1774-46d7-b469-109ddec68cd")).toBe(false);
+  });
+
+  describe("when taking an optional version parameter", () => {
+    it("can return true or false for version 1", () => {
+      expect(
+        checkIsGuid("f4090a94-1774-16d7-b469-109ddec68cd2", { version: 1 }),
+      ).toBe(true);
+      expect(
+        checkIsGuid("f4090a94-1774-46d7-b469-109ddec68cd2", { version: 1 }),
+      ).toBe(false);
+    });
+
+    it("can return true or false for version 2", () => {
+      expect(
+        checkIsGuid("f4090a94-1774-26d7-b469-109ddec68cd2", { version: 2 }),
+      ).toBe(true);
+      expect(
+        checkIsGuid("f4090a94-1774-46d7-b469-109ddec68cd2", { version: 2 }),
+      ).toBe(false);
+    });
+
+    it("can return true or false for version 3", () => {
+      expect(
+        checkIsGuid("f4090a94-1774-36d7-b469-109ddec68cd2", { version: 3 }),
+      ).toBe(true);
+      expect(
+        checkIsGuid("f4090a94-1774-46d7-b469-109ddec68cd2", { version: 3 }),
+      ).toBe(false);
+    });
+
+    it("can return true or false for version 4", () => {
+      expect(
+        checkIsGuid("f4090a94-1774-46d7-b469-109ddec68cd2", { version: 4 }),
+      ).toBe(true);
+      expect(
+        checkIsGuid("f4090a94-1774-56d7-b469-109ddec68cd2", { version: 4 }),
+      ).toBe(false);
+    });
+
+    it("can return true or false for version 5", () => {
+      expect(
+        checkIsGuid("f4090a94-1774-56d7-b469-109ddec68cd2", { version: 5 }),
+      ).toBe(true);
+      expect(
+        checkIsGuid("f4090a94-1774-46d7-b469-109ddec68cd2", { version: 5 }),
+      ).toBe(false);
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-const checkIsGuid = (value: string): boolean => {
+const checkIsGuid = (
+  value: string,
+  options?: { version?: number },
+): boolean => {
   const guidRegex = new RegExp(
-    "^[0-9a-f]{8}(-[0-9a-f]{4}){4}[0-9a-f]{8}$",
+    `^[0-9a-f]{8}-[0-9a-f]{4}-[${
+      options?.version ? options.version : "1-5"
+    }][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$`,
     "i",
   );
   return guidRegex.test(value);
